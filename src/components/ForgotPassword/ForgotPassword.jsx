e2364946e5ca9ab6c46980026c2abe2369f87fb0
Nwill120 <nkosiwilliamsjr@gmail.com>
2023-12-02 16:42:14 -0500

    Update ForgotPassword.jsx

--- src/components/ForgotPassword/ForgotPassword.jsx
+++ src/components/ForgotPassword/ForgotPassword.jsx
@@ -5,47 +5,46 @@ const ForgotPassword = ({ setUser }) => {
   const [email, setEmail] = useState("");
   const [openChangePassword, setOpenChangePassword] = useState(false);
 
-  const submitHandler = (obj) => {
-    const options = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: obj.email,
-      }),
-    };
+  const sendResetLink = async (userEmail) => {
+    try {
+    // reset email link
+      const response = await fetch("http://localhost:8000/SendResetLink", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email: userEmail }),
+      });
 
-    fetch("http://localhost:8000/LogIn", options).then((res) => {
-      res
-        .json()
-        .then((data) => ({
-          data: data,
-          status: res.status,
-        }))
-        // eslint-disable-next-line no-unused-vars
-        .then((res) => {});
-    });
-    setOpenChangePassword(true);
+      if (response.ok) {
+        setOpenChangePassword(true);
+      } else {
+        console.error("Password Reset Link Failed To Send, Please Try Again");
+       
+      }
+    } catch (error) {
+      console.error("Password Reset Link encountered error, Please review email entry and try again", error);
+         }
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const formData = new FormData(e.target);
+    const obj = {
+      email: formData.get("email") || "",
+    };
+    setEmail(obj.email);
+    sendResetLink(obj.email);
   };
 
   if (!openChangePassword) {
     return (
-      <form
-        onSubmit={(e) => {
-          e.preventDefault();
-          const formData = new FormData(e.target);
-          const obj = {
-            email: formData.get("email") ?? "",
-          };
-          setEmail(formData.get("email") ?? "");
-          submitHandler(obj);
-        }}
-      >
+      <form 
+         onSubmit={handleSubmit}>
         <h1>Forgot Your Password?</h1>
-        <p>Enter Your Email!</p>
+        <p>Enter Your Email</p>
         <input type="text" name="email" id="email" />
-        <button>Submit</button>
+        <button type="submit">Submit</button>
       </form>
     );
   } else {
